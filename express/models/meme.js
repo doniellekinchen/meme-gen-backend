af6151cd8312e4648a39c60fa4d19ad7312fa418
doniellekinchen <donicodes.things@gmail.com>
2023-12-15 15:25:51 -0500

    add image to schema

--- express/models/meme.js
+++ express/models/meme.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 const MemeSchema = new Schema ({
     caption1: { type: String, required: true },
     caption2: { type: String, required: true },
+    image: { type: String, require: true},
 })
 
 
